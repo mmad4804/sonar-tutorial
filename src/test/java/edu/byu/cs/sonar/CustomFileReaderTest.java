963a22000bf182ab958240c56010b1d4faa4ec0d
Micaela Madariaga <madariagamica20@gmail.com>
2026-04-01 23:29:57 -0600

    Increased code coverage for equals()

--- src/test/java/edu/byu/cs/sonar/CustomFileReaderTest.java
+++ src/test/java/edu/byu/cs/sonar/CustomFileReaderTest.java
@@ -9,38 +9,65 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomFileReaderTest {
 
-    private CustomFileReader sut;
+  private CustomFileReader sut;
 
-    @BeforeEach
-    void setUp() {
-        sut = new CustomFileReader("readMe1.txt");
-    }
+  @BeforeEach
+  void setUp() {
+    sut = new CustomFileReader("readMe1.txt");
+  }
 
-    @Test
-    void testHowManyWordsInFile() throws FileNotFoundException {
-        assertEquals(4, sut.howManyWordsInFile(), "There should be 4 words in readMe1.txt");
-    }
+  @Test
+  void testHowManyWordsInFile() throws FileNotFoundException {
+    assertEquals(4, sut.howManyWordsInFile(), "There should be 4 words in readMe1.txt");
+  }
 
-    @Test
-    void testReturnThatWord() throws FileNotFoundException {
-        assertEquals("I", sut.returnThatWord(1), "The first word should be I in readMe1.txt");
-    }
+  @Test
+  void testReturnThatWord() throws FileNotFoundException {
+    assertEquals("I", sut.returnThatWord(1), "The first word should be I in readMe1.txt");
+  }
 
-    @Test
-    void testFindNewWord() throws FileNotFoundException {
-        sut.findNewWord("C");
-        assertEquals("Computer ", sut.getNewSentence(), "Computer should be found when C queried");
-    }
+  @Test
+  void testFindNewWord() throws FileNotFoundException {
+    sut.findNewWord("C");
+    assertEquals("Computer ", sut.getNewSentence(), "Computer should be found when C queried");
+  }
 
-    @Test
-    void testGetNewSentence() {
-        assertEquals("", sut.getNewSentence(), "New sentence should be empty initially");
-    }
+  @Test
+  void testGetNewSentence() {
+    assertEquals("", sut.getNewSentence(), "New sentence should be empty initially");
+  }
 
-    @Test
-    void setNewSentence() {
-        String betterSentence = "New Sentence.";
-        sut.setNewSentence(betterSentence);
-        assertEquals(betterSentence, sut.getNewSentence());
+  @Test
+  void setNewSentence() {
+    String betterSentence = "New Sentence.";
+    sut.setNewSentence(betterSentence);
+    assertEquals(betterSentence, sut.getNewSentence());
+  }
+
+  @Test
+  void testEquals() {
+    CustomFileReader sameAsSut = new CustomFileReader("readMe1.txt");
+    assertNotEquals(null, sut);
+    assertNotEquals("I am a string, not a reader", sut);
+    assertEquals(sut, sut);
+    assertEquals(sameAsSut, sut);
+  }
+
+  @Test
+  void testFalseEquals() {
+    CustomFileReader diffSentence = new CustomFileReader("readMe1.txt");
+    diffSentence.setNewSentence("Something different");
+    assertNotEquals(sut, diffSentence);
+
+    CustomFileReader diffCount = new CustomFileReader("readMe1.txt");
+    try {
+      diffCount.howManyWordsInFile();
+    } catch (FileNotFoundException e) {
+      fail("File should exist");
     }
+    assertNotEquals(sut, diffCount);
+
+    CustomFileReader diffPath = new CustomFileReader("other.txt");
+    assertNotEquals(sut, diffPath);
+  }
 }
